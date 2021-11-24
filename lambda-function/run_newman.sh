6c69747507b86acd35c5079f9c02e222aca9f61b
abhilashusha <abhilash.u.nair@gmail.com>
2021-11-24 18:48:06 -0500

    Added invalid python

--- lambda-function/run_newman.sh
+++ lambda-function/run_newman.sh
@@ -1,6 +1,6 @@
 stages=`cat terraformapply.out | jq '.stage_names.value[]'`
 url=`cat terraformapply.out | jq '.base_urls1.value'`
-echo $url
+
 for stage in $stages
 do
  var2=`sed -e 's/^"//' -e 's/"$//' <<<"$stage"`
@@ -8,6 +8,6 @@ do
 
  echo '{"values": [{"key": "api_url","value":"'$url1$var2'"}]}' > $var2.txt
  newman run empty.json -e $var2.txt
- echo $stage
+ 
 
 done
